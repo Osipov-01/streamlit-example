f2575463ad7f9776e01dbe95da3494e8d8c4348d
Osipov-01 <118967934+Osipov-01@users.noreply.github.com>
2023-02-09 17:30:50 +0300

    Update SM-liiga.py

--- SM-liiga.py
+++ SM-liiga.py
@@ -3,6 +3,8 @@ import streamlit as st
 
 #data = pd.read_csv('C:/Users/User/Desktop/SM-liiga/SM-liiga.csv', encoding='cp1251', sep=';')
 
+a,b = st.multi_slider("Select Price Range:", 1, 16, step = 1)
+
 st.subheader('Input xlsx file')
 uploaded_file = st.file_uploader("Choose a file")
 
@@ -23,7 +25,7 @@ if uploaded_file is not None:
 #data1 = pd.read_excel(C:/Users/User/Desktop/SM-liiga/SM-liiga.xlsx'')
                                                                                              
 
-    st.dataframe(data)
+    st.dataframe(data[a:b])
     print(data.columns)
     #st.table(data.columns)
 
